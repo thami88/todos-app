53d69d3aba9a5b875007c8e19f71b62a06112f0e
thami88 <thaminduliyanage97@gmail.com>
2024-06-04 14:59:07 +0530

    [ADDED] PageHomeTest CourseModel CourseFactory View  and PageHomeController

---
+++ app/Http/Controllers/PageHomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class PageHomeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $courses = Course::all();
+        return view('home', compact('courses'));
+    }
+}
