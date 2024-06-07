00cadf63efcdc0e43e2844b860e39b5a796a467c
thami88 <thaminduliyanage97@gmail.com>
2024-06-07 19:41:59 +0530

    [FIX] use Laravel Pint, a code style fixer

--- tests/Feature/PageResponseTest.php
+++ tests/Feature/PageResponseTest.php
@@ -1,10 +1,10 @@
 <?php
 
-use function Pest\Laravel\get;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-uses(RefreshDatabase::class);
+use function Pest\Laravel\get;
 
+uses(RefreshDatabase::class);
 
 it('gives back successful response for home page', function () {
     // Act & Assert
