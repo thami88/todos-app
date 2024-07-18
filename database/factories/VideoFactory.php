9b1750dd1d567963b633a0a16c8d6627ce335373
thami88 <thaminduliyanage97@gmail.com>
2024-07-18 12:05:45 +0530

    [REFACTOR] videoTest

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Course;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,11 +18,12 @@ class VideoFactory extends Factory
     public function definition(): array
     {
         return [
+            'course_id' => Course::factory(),
             'slug' => $this->faker->slug,
             'vimeo_id' => $this->faker->uuid,
             'title' => $this->faker->sentence,
             'description' => $this->faker->paragraph,
-            'duration' => $this->faker->numberBetween(1, 99)
+            'duration_in_mins' => $this->faker->numberBetween(1, 99)
         ];
     }
 }
